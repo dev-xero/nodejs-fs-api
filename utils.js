03dce3f3267ac3667800bbaedb7c8241619b20b7
dev-xero <elo48858@gmail.com>
2024-10-17 13:21:36 +0100

    fix: Correctly export parse bondary util function

--- utils.js
+++ utils.js
@@ -1,6 +1,6 @@
 const parseBoundary = (header) => {
   const matches = header.match(/boundary=(.+)$/);
   return matches && matches[1] ? matches[1] : null;
-}
+};
 
-modules.exports = { parseBoundary }
+module.exports = { parseBoundary };
